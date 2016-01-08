237703d106311bc1f8c7634a182abf69a05f5127
molgin <mollygingras@gmail.com>
2016-01-07 16:19:52 -0800

    palindromes again

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,36 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSString *lowercase = [string lowercaseString];
+    
+    // because i felt like it
+    NSRegularExpression *alphaOnlyRegex = [NSRegularExpression regularExpressionWithPattern:@"[^a-z]"
+                                                                                    options:NSRegularExpressionCaseInsensitive
+                                                                                      error:NULL];
+    
+    NSString *alphabeticalString = [alphaOnlyRegex stringByReplacingMatchesInString:lowercase options:0 range:NSMakeRange(0, [lowercase length]) withTemplate:@""];
+    
+    NSString *reversedString = [self stringByReversingString:alphabeticalString];
+    
+    BOOL isPalindrome = [alphabeticalString isEqualToString:reversedString];
+    
+    return isPalindrome;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+    }
+    
+    return result;
+}
+
 
 @end
